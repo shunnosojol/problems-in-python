f30e5c6ecea4e93a9ca45fc121f7391d7b0d202a
Taulut Hossain Washi <washinpi@gmail.com>
2017-05-14 01:30:50 +0600

    Day 1: Data Types

---
+++ 2.data-type.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/30-data-types
+
+# Declare second integer, double, and String variables.
+input_int = input()
+# Read and save an integer, double, and String to your variables.
+input_double = input()
+input_string = input()
+# Print the sum of both integer variables on a new line.
+print(i+int(input_int))
+print(d+float(input_double))
+
+# Print the sum of the double variables on a new line.
+print(s+input_string)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
