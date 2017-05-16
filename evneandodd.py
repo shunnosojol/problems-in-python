34b8ade9f39b096735d2eecbb2513e8e5def0916
Taulut Hossain Washi <washinpi@gmail.com>
2017-05-16 22:14:34 +0600

    Day 3: Intro to Conditional Statements

---
+++ evneandodd.py
@@ -0,0 +1,13 @@
+
+
+n = 25
+if n>=6 and n<=20:
+    print("Weird")
+elif n==4:
+    print("Not Weird")
+elif n%2==1:
+    print("Weird")
+elif n%2==0:
+    print("Not Weird")
+else:
+   print("Not Weird") 
