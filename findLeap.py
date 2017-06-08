25f4258fdc3024751754290744c3231b30ff8cef
Taulut Hossain Washi <washinpi@gmail.com>
2017-06-09 03:03:22 +0600

    Check leap year

---
+++ findLeap.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/write-a-function
+def is_leap(year):
+    leap = False
+
+    # Write your logic here
+    if (year % 4) == 0 and (year % 100) != 0 or year % 400 == 0:
+            leap = True
+
+    return leap
+print(is_leap(1990))
+
+
+
+
+
