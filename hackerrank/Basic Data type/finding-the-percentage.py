115fee90bd51bea30423c9eea11586bc843654ab
Taulut Hossain Washi <washinpi@gmail.com>
2017-10-26 16:47:48 +0600

    Finding the percentage

---
+++ hackerrank/Basic Data type/finding-the-percentage.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+   n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    if query_name in student_marks:
+        print("{0:.2f}".format(sum(student_marks[query_name])/len(student_marks[query_name])))
