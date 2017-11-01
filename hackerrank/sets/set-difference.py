b8d0a89678df44bbd2923d3c8f6b32121d3ed053
Taulut Hossain Washi <washinpi@gmail.com>
2017-11-01 22:27:03 +0600

    Set .difference() Operation

---
+++ hackerrank/sets/set-difference.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+n = int(input())
+role_en = set(map(int,input().split()))
+b = int(input())
+role_french = set(map(int,input().split()))
+print(len(role_en - role_french))
